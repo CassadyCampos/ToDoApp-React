c27fd3819cce700e14bdd73acb5fa861af1598b3
Cassady Campos <44253571+CassadyCampos@users.noreply.github.com>
2019-05-01 16:20:23 -0600

    Styling

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+function App() {
+    return (
+        <div id='container'>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+import './components/App.css'
+
+//* Time 48:52
+//Render an <App /> component
+//Create the <App /> component from scratch
+//Have the <App />  component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard coded items on it
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
